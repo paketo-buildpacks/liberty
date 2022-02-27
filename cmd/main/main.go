e68c287220ae733e0b656347788b1bf1521fb529
Kevin Ortega <ortegake@us.ibm.com>
2022-02-27 14:53:37 -0500

    Update cmd/buildpack/main.go
    Co-authored-by: Daniel Mikusa <dmikusa@vmware.com>

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -21,7 +21,7 @@ import (
 
 	"github.com/paketo-buildpacks/libpak"
 	"github.com/paketo-buildpacks/libpak/bard"
-	"github.com/paketo-buildpacks/liberty/openliberty"
+	"github.com/paketo-buildpacks/liberty/liberty"
 )
 
 func main() {
