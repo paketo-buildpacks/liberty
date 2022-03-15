075b1003939869adec26ec62b3475d77564f00d6
Kevin Ortega <ortegake@us.ibm.com>
2022-03-15 16:00:28 -0400

    Adding support to install features and ifixes

--- liberty/base.go
+++ liberty/base.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package liberty
 
 import (
@@ -8,10 +24,12 @@ import (
 	"github.com/paketo-buildpacks/libpak"
 	"github.com/paketo-buildpacks/libpak/bard"
 	"github.com/paketo-buildpacks/libpak/sherpa"
+	"github.com/paketo-buildpacks/libpak/effect"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 )
 
 type Base struct {
@@ -67,6 +85,28 @@ func (b Base) Contribute(layer libcnb.Layer) (libcnb.Layer, error) {
 		if err := b.ContributeUserFeatures(layer); err != nil {
 			return libcnb.Layer{}, fmt.Errorf("unable to contribute user features\n%w", err)
 		}
+					
+		listoffeatures, _ := b.ConfigurationResolver.Resolve("BP_LIBERTY_FEATURES")
+		features := strings.Fields(listoffeatures)
+		
+		profile, _ := b.ConfigurationResolver.Resolve("BP_LIBERTY_PROFILE")
+		installationLocation := fmt.Sprintf("/layers/paketo-buildpacks_liberty/open-liberty-runtime-%s", profile)
+		
+		for  _, feature := range features {
+			b.Logger.Info("Installing ", feature)
+				
+			executor := effect.NewExecutor()
+			if err := executor.Execute(effect.Execution{
+				Command: filepath.Join(installationLocation,"bin","featureUtility"),
+				Args:    []string{"installFeature", feature, "--acceptLicense"},
+				Dir:     layer.Path,
+				Stdout:  bard.NewWriter(b.Logger.InfoWriter(), bard.WithIndent(3)),
+				Stderr:  bard.NewWriter(b.Logger.InfoWriter(), bard.WithIndent(3)),
+			}); err != nil {
+				return libcnb.Layer{}, fmt.Errorf("unable to apply ifix\n%w", err)
+			}
+		}		
+
 		layer.LaunchEnvironment.Default("BPI_LIBERTY_BASE_ROOT", layer.Path)
 		layer.LaunchEnvironment.Default("BPI_LIBERTY_SERVER_NAME", b.ServerName)
 
@@ -100,6 +140,29 @@ func (b Base) ContributeExternalConfiguration(layer libcnb.Layer) error {
 	if err := util.Extract(artifact, confPath, c); err != nil {
 		return fmt.Errorf("unable to expand external configuration\n%w", err)
 	}
+	
+	fixes, error := ioutil.ReadDir(filepath.Join(confPath, "ifixes"))
+	profile, _ := b.ConfigurationResolver.Resolve("BP_LIBERTY_PROFILE")
+	installationLocation := fmt.Sprintf("/layers/paketo-buildpacks_liberty/open-liberty-runtime-%s", profile)
+
+	for _, fix := range fixes {
+		b.Logger.Info("Installing ", fix.Name())
+		ifix := filepath.Join(confPath, "ifixes", fix.Name())
+		executor := effect.NewExecutor()
+		if err := executor.Execute(effect.Execution{
+			Command: filepath.Join("java"),
+			Args:    []string{"-jar", ifix, "--installLocation", installationLocation},
+			Dir:     layer.Path,
+			Stdout:  bard.NewWriter(b.Logger.InfoWriter(), bard.WithIndent(3)),
+			Stderr:  bard.NewWriter(b.Logger.InfoWriter(), bard.WithIndent(3)),
+		}); err != nil {
+			return fmt.Errorf("unable to apply ifix\n%w", err)
+		}
+	}
+		
+	if error != nil {
+		b.Logger.Debug("error reading dir");
+	}	
 
 	return nil
 }
